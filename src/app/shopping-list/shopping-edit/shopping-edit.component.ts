cb53b731c23da92eb783927cbd39cf5ffe9c9a5b
Ruijie218 <ruijie218@hotmail.com>
2021-06-15 00:18:13 +0800

    Added directive, property and event binding

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ViewChild, ElementRef} from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput') nameInputRef : ElementRef;
+  @ViewChild('amtInput', {static:false}) amtInputRef : ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
+  ingredient: Ingredient;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onAddIngredient() {
+    const inputName = this.nameInputRef.nativeElement.value;
+    const inputAmt = this.amtInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(inputName, inputAmt );
+    this.ingredientAdded.emit(newIngredient);
   }
 
 }
